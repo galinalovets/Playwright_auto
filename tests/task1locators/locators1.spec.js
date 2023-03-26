852d9e94a486553cff2bdc47e12560836407bf10
Halina Lavets <lovets.galina@gmail.com>
2023-03-26 10:23:58 +0300

    add tests

--- tests/task1locators/locators1.spec.js
+++ tests/task1locators/locators1.spec.js
@@ -1,28 +1,39 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('onliner clever link', async ({ page }) => {
+test('locators', async ({ page }) => {
   await page.goto('https://www.onliner.by/');
+   
+  // screenshots - go to the img folder
+  await page.locator('.onliner_logo').screenshot({path: 'img/task1.jpeg'});
+  await page.locator('xpath=//input[@class="fast-search__input"]').screenshot({path: 'img/task2.jpeg'});
+  await page.getByRole('link', { name: 'Onlíner Клевер' }).screenshot({path: 'img/task3.jpeg'});
+  await page.getByText('Умные часы и браслеты').screenshot({path: 'img/task4.jpeg'});
+  await page.locator('.project-navigation__item'); //list of elements
+  await page.locator('.project-navigation__item').nth(1).screenshot({path: 'img/task5.jpeg'});
+  await page.getByPlaceholder("Поиск в Каталоге. Например, ").screenshot({path: 'img/task6.jpeg'});
+  await page.getByAltText('Onlíner').screenshot({path: 'img/task7.jpeg'});
+  await page.getByTitle('Корзина').screenshot({path : 'img/task8.jpeg'});
+  await page.screenshot({path: 'img/taskpage.jpeg'});
   
-  //click the onliner клевер link
-  await page.getByRole('link', { name: 'Onlíner Клевер' }).click();
-    
 });
 
-test('onliner best text', async ({ page }) => {
-  await page.goto('https://www.onliner.by/');
+test('forum locators', async ({ page }) => {
+  await page.goto('https://forum.onliner.by/');
 
-  // Expects the element to contain Onlíner. Лучшее.
-  await expect(page.getByText('Onlíner. Лучшее')).toBeVisible();
+  await page.locator('.h-hottopics').screenshot({path : 'img/task9.jpeg'});
 
-});
-
-test('input text over placeholder', async ({ page }) => {
-  await page.goto('https://www.onliner.by/');
-
-  // 
   await page
-    .getByPlaceholder("Поиск в Каталоге. Например, \"садовые качели\"")
-    .fill("велосипед");
+    .getByRole('listitem')
+    .filter({ hasText: /Бюро находок/ })
+    .getByText('226')
+    .screenshot({path : 'img/task10.jpeg'});
 
-});
+  
+  await page
+    .getByRole('listitem')
+    .filter({ has: page.getByText('Благотворительность. Им нужна помощь!')})
+    .getByRole('link').last()
+    .screenshot({path : 'img/task11.jpeg'});
+ 
+})
